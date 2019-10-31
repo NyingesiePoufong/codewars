404faa25ed1faac67aee4e3838b8e65aaee90b0d
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-10-30 22:21:26 -0400

    adding new codewar solution

---
+++ CodeWars8kyu_15/comboStrings.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/grasshopper-combine-strings/train/javascript
+
+// Create the combineNames function here
+
+function combineNames(name1, name2){
+  return `${name1} ${name2}`
+}
