c6d5ea518b7ff49545e03ffc20d78ec5f0fd7796
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-10-31 23:59:05 -0400

    adding new code

---
+++ CodeWars8kyu_16/createRoom.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/grasshopper-create-the-rooms/train/javascript
+
+// Add rooms here
+var rooms = {
+  room1:{
+    name:'One',
+    description:'this is room one',
+    completed: 0
+  },
+  room2:{
+    name:'Two',
+    description:'this is room two',
+    completed: 0
+  },
+  room3:{
+    name:'Three',
+    description:'this is room one',
+    completed: 0
+  }
+}
