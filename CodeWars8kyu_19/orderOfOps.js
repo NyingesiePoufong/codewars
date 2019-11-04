65fc0a0989c7d9f591ca749bd94e2380d32b5d8e
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-03 23:17:26 -0500

    adding new codewar solution

---
+++ CodeWars8kyu_19/orderOfOps.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/grasshopper-order-of-operations/train/javascript
+//make the function return 32 instead of 10
+
+function orderOperations () {
+  return (2 + 2) * ((2 + 2) * 2)
+}
