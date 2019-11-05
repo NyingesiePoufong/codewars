8bcbc98539f822dfed448a6d5bdc83e61da91f09
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-05 11:57:40 -0500

    adding new code war solution 21

---
+++ CodeWars8kyu_21/terminalGame.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/grasshopper-terminal-game-number-1/train/javascript
+
+function Hero (name) {
+  // add default values here
+
+    this.name = name || 'Hero',
+    this.position = '00',
+    this.health = 100,
+    this.damage = 5,
+    this.experience = 0
+
+
+}
