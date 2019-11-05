d0e234b9bef6bbd6c86b11bce8bd711f98931640
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-04 23:17:36 -0500

    adding new coing solution 20

---
+++ CodeWars8kyu_20/shoppingList.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/grasshopper-shopping-list/train/javascript
+
+
+//Create variables here
+
+
+const sandwiches = 4;
+const salads = 6;
+const wraps = 5;
+const frenchFries = 10;
+
+let totalPrice = (sandwiches * 8) + (salads * 7) + (wraps * 6.50) + (frenchFries * 1.20)
