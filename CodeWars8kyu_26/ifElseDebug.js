8bea8ecbbc9bb093876b0aaa71142857c4734ce7
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-10 23:16:56 -0500

    adding codewar solution 26

---
+++ CodeWars8kyu_26/ifElseDebug.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/grasshopper-if-slash-else-syntax-debug/train/javascript
+
+function checkAlive (health) {
+  if(health <= 0){
+    return false
+  }else{
+    return true
+  }
+}
