bc4867cdd692750f7a03a828a7f795b24d947529
NyingesiePoufong <nyingesie.poufong.dev@gmail.com>
2019-11-12 21:49:46 -0500

    adding codewar solution 28

---
+++ CodeWars8kyu_28/superEasy.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/super-duper-easy/train/javascript
+
+function problem(x){
+ let math = x
+
+ if (typeof x !== "number"){
+   return 'Error'
+ }else{
+   return ((x * 50) + 6)
+ }
+}
